48cb68456e41653b20e9d252684daf41bc3b13f3
imasj-git <joshisaurav044@gamil.com>
2024-12-19 21:27:09 +0545

    theme added

---
+++ lib/core/aap_theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    primarySwatch: Colors.orange,
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: 'Lora Regular',
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:bus_ticket_app/core/aap_theme/app_theme.dart';
 import 'package:bus_ticket_app/view/home_view.dart';
 import 'package:bus_ticket_app/view/landing_page_view.dart';
 import 'package:bus_ticket_app/view/login_view.dart';
-import 'package:bus_ticket_app/view/signup_view.dart';
 import 'package:bus_ticket_app/view/onboarding_screen.dart';
+import 'package:bus_ticket_app/view/signup_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,7 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
+      theme: getApplicationTheme(),
       routes: {
         '/': (context) => const LandingPageView(),
         '/starting': (context) => const OnboardingScreen(),
