0334e1cfaa26a9315aa70d2c90816671defb75e5
imasj-git <joshisaurav044@gamil.com>
2024-12-20 18:28:28 +0545

    changes

--- lib/view/bus_view.dart
+++ lib/view/bus_view.dart
@@ -7,6 +7,7 @@ class BusView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: const Text(
             'Bus',
             style: TextStyle(color: Colors.white),
