1430e3d81373f45b2c86ec28516b28d550cd3908
imasj-git <joshisaurav044@gamil.com>
2025-01-17 14:15:31 +0545

    theme added

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,36 +1,67 @@
+import 'package:bus_ticket_application/app/constants/theme_constant.dart';
 import 'package:flutter/material.dart';
 
-ThemeData getApplicationTheme() {
-  return ThemeData(
-    primarySwatch: Colors.deepPurple,
-    scaffoldBackgroundColor: Colors.white,
-    fontFamily: 'Lora Regular',
-    textTheme: const TextTheme(
-      bodyMedium: TextStyle(
-        fontSize: 16,
-        fontFamily: 'Lora Regular',
-      ),
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        textStyle: const TextStyle(
-            fontSize: 18,
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
-            fontFamily: 'Lora Bold'),
-        backgroundColor: Colors.deepPurple,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
+class AppTheme {
+  AppTheme._();
+
+  static getApplicationTheme({required bool isDarkMode}) {
+    return ThemeData(
+      // Change app bar color
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        backgroundColor: ThemeConstant.appBarColor,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
         ),
       ),
-    ),
-    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-      selectedLabelStyle: TextStyle(
-        fontFamily: 'Lora Regular',
+
+      // Change elevated button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          foregroundColor: Colors.white,
+          backgroundColor: ThemeConstant.primaryColor,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
       ),
-      unselectedLabelStyle: TextStyle(
-        fontFamily: 'Lora Regular',
+
+      // Change text field theme
+      inputDecorationTheme: const InputDecorationTheme(
+        contentPadding: EdgeInsets.all(15),
+        border: OutlineInputBorder(),
+        labelStyle: TextStyle(
+          fontSize: 20,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.red,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: ThemeConstant.primaryColor,
+          ),
+        ),
       ),
-    ),
-  );
+      // Circular progress bar theme
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: ThemeConstant.primaryColor,
+      ),
+      //Bottom navigation bar theme
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+        selectedItemColor: Color(0xFFFE5404),
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        elevation: 0,
+      ),
+    );
+  }
 }
